0a96cebf4907436f87713ece5e4b2bd9776c7808
DouglasPenn23 <Douglaspennington2013@gmail.com>
2022-10-12 20:53:46 -0400

    Assorted Changes

--- vindication/src/App.js
+++ vindication/src/App.js
@@ -7,18 +7,7 @@ function App() {
     <div className="App">
       <Navigation />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit your butt and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Hello </h1>
       </header>
     </div>
   );
